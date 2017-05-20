2e0d5cf168a05e8cb50630713bb11ce00f0b70ad
Karl <sackboysteve1@gmail.com>
2017-05-20 17:40:06 +0200

    Fixed start.sh (hopefully)
    This should fix problems for some people, otherwise fml

--- start.bat
+++ start.bat
@@ -1,11 +1,5 @@
 @echo off
 title Music Bot
 echo Starting Bot
-python --version > NUL 2>&1
-IF %ERRORLEVEL% NEQ 0 GOTO nopython
 python "main.py"
-
-:nopython
-echo ERROR - Python is missing!
-:end
 pause
